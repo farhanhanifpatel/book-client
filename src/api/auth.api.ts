e62a58d7c3c058ea206b279dc196587fd92932c0
iamfarhanpatel <patelfarhan459l@example.com>
2026-05-07 18:49:18 +0530

    add local storage.

--- src/api/auth.api.ts
+++ src/api/auth.api.ts
@@ -13,11 +13,19 @@ export interface RegisterPayload {
 
 export const login = async (payload: LoginPayload) => {
   const res = await api.post("/auth/login", payload);
+
+  // 🔥 SAVE TOKEN (IMPORTANT)
+  localStorage.setItem("token", res.data.data.accessToken);
+
   return res.data;
 };
 
 export const register = async (payload: RegisterPayload) => {
   const res = await api.post("/auth/signup", payload);
+
+  // optional auto-login after signup
+  localStorage.setItem("token", res.data.data.accessToken);
+
   return res.data;
 };
 
@@ -27,5 +35,5 @@ export const getProfile = async () => {
 };
 
 export const logout = async () => {
-  await api.post("/auth/logout");
+  localStorage.removeItem("token");
 };
